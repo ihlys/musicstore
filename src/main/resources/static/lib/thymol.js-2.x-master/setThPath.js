8a9a06d610b33d9a8c4f609a44d681c7f2b0fd72
Ihor <ihorlysachenko@gmail.com>
2017-05-03 16:22:37 +0300

    Added thymol js library for fragments static prototyping

---
+++ src/main/resources/static/lib/thymol.js-2.x-master/setThPath.js
@@ -0,0 +1,4 @@
+var	thRoot="D:/MyData/Игорек/DocumentsAll/Studies/Java/Projects/MusicStore/musicstore/src/main/resources/static";		// In an exploded war, this is usually webapp root
+var	thPath="";		// In an exploded war, this is usually WEB-INF/<some-path>
+var	thProtocol="";
+var thDebug = true;
