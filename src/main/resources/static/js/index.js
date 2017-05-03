9c3de6a91306e0585605afd7da031bddd4a2235f
Ihor <ihorlysachenko@gmail.com>
2017-05-03 18:41:04 +0300

    Save editions in markup

--- src/main/resources/static/js/index.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function() {
-
-	$("button.music-entity-info__expand-btn").click(function() {
-		$(".music-entity-info_details").slideToggle("slow");
-	});
-	
-});
---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function() {
+
+	$("button.music-info__expand-btn").click(function() {
+		$(".music-info__details").slideToggle("slow");
+	});
+	
+});
