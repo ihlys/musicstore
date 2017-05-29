047f2692b40721ed9dd8537bd9ac24584ed0bbdf
Ihor <ihorlysachenko@gmail.com>
2017-05-29 17:23:34 +0300

    flex-last-row jquery plugin fixes

--- src/main/resources/static/js/jquery.flexbox-last-row-fix.js
+++ src/main/resources/static/js/jquery.flexbox-last-row-fix.js
@@ -2,51 +2,97 @@
   
   $.fn.fixFlexBoxLastRow = function() {
     
-    return this.each(() => {
-      let flexedDiv = $(this);
-      let flexSpaceAroundHelper = new FlexSpaceAroundHelper(flexedDiv);
+    return this.each(function() {
+      let $flexedDiv = $(this);
+      let flexSpaceAroundHelper = new FlexSpaceAroundHelper($flexedDiv);
+      
+      let observer = new MutationObserver(function(mutations) {
+        if (mutations.some(mutation => mutation.type === 'childList')) {
+          flexSpaceAroundHelper.refresh();
+        }
+      });
+      
+      let flexedDiv = $(this)[0];
+      observer.observe(flexedDiv, { childList: true });
       
       let sensor = new ResizeSensor(flexedDiv, function() {
-        flexSpaceAroundHelper.squeezeChildrenToLeftInLastRow();
+          flexSpaceAroundHelper.squeezeChildrenToLeftInLastRow();
       });
       
       flexSpaceAroundHelper.squeezeChildrenToLeftInLastRow();
     });
   }
   
-  var FlexSpaceAroundHelper = function FlexSpaceAroundHelper( div ) {
-    let divChildren = div.children(),
-      lastChildren = divChildren.last(),
-      childrenCount = divChildren.length,
-      childrenMarginRight = parseInt(lastChildren.css("marginRight")),
-      childrenHorizontalSpace = parseInt(lastChildren.css("width")) + childrenMarginRight + parseInt(lastChildren.css("marginLeft"));
+  
+  var FlexSpaceAroundHelper = (function() {
     
-    this.squeezeChildrenToLeftInLastRow = function() {
+    var flexedDiv = Symbol('flexedDiv');
+    var divChildren = Symbol('divChildren');
+    var lastChild = Symbol('lastChild');
+    var childrenCount = Symbol('childrenCount');
+    var childMarginRight = Symbol('childMarginRight');
+    var childHorizontalSpace = Symbol('childHorizontalSpace');
+    
+    function init(instance) {
+      instance[divChildren] = instance[flexedDiv].children(":not(.resize-sensor)");
+      instance[lastChild] = instance[divChildren].last();
+      instance[childrenCount] = instance[divChildren].length;
+      instance[childMarginRight] = parseInt(instance[lastChild].css("marginRight"));
+      instance[childHorizontalSpace] = parseInt(instance[lastChild].css("width")) + instance[childMarginRight] +
+                  parseInt(instance[lastChild].css("marginLeft"));
       
-      let divWidth = div.width();
-        childrenInOneRow = Math.floor(divWidth / childrenHorizontalSpace);
-        missingChildrenInLastRow = childrenInOneRow - childrenCount % childrenInOneRow;
-        isRowFull = missingChildrenInLastRow < childrenInOneRow;
-        marginRestored = false;
-        
-      if ( isRowFull ) {
-        let gapsExtraSpacePerChildren = (divWidth - childrenInOneRow * childrenHorizontalSpace) / childrenInOneRow,
-          freeSpaceLeft = missingChildrenInLastRow * (childrenHorizontalSpace + gapsExtraSpacePerChildren);
+      console.log(`FlexSpaceAroundHelper(init):\n` +
+                  `childrenCount = ${instance[childrenCount]};\n` +
+                  `childMarginRight = ${instance[childMarginRight]};\n` +
+                  `childHorizontalSpace = ${instance[childHorizontalSpace]};\n`);
+    }
+    
+    function restoreMargin(instance) {
+      instance[lastChild].css("marginRight", instance[childMarginRight]);
+    }
+    
+    class FlexSpaceAroundHelper {
       
-        lastChildren.css("marginRight", freeSpaceLeft + childrenMarginRight);
+      constructor( div ) {
+        this[flexedDiv] = div;
+        init(this);
+      }
+     
+      
+      refresh() {
+        restoreMargin(this);
+        init(this);
+        this.squeezeChildrenToLeftInLastRow();
+      }
+      
+      squeezeChildrenToLeftInLastRow() {
+        let divWidth = this[flexedDiv].width(),
+          childrenInOneRow = Math.floor(divWidth / this[childHorizontalSpace]),
+          missingChildrenInLastRow = childrenInOneRow - this[childrenCount] % childrenInOneRow,
+          marginRestored = false;
+          
+          console.log("sdfsd " + childrenInOneRow);
+      
+        if ( missingChildrenInLastRow < childrenInOneRow ) {
+          let gapsExtraSpacePerChildren = (divWidth - childrenInOneRow * this[childHorizontalSpace]) / childrenInOneRow,
+            freeSpaceLeft = missingChildrenInLastRow * (this[childHorizontalSpace] + gapsExtraSpacePerChildren);
         
-        console.log(`FlexSpaceAroundHelper:\n` +
-                    `divWidth = ${divWidth};\n` +
-                    `childrenHorizontalSpace = ${childrenHorizontalSpace};\n` +
-                    `childrenInOneRow = ${childrenInOneRow};\n` +
-                    `gapsExtraSpacePerChildren = ${gapsExtraSpacePerChildren};\n` +
-                    `missingChildrenInLastRow = ${missingChildrenInLastRow}\n` +
-                    `freeSpaceLeft = ${freeSpaceLeft}`);
-      } else if ( !marginRestored ) {
-        lastChildren.css("marginRight", childrenMarginRight);
-        marginRestored = true;
+          this[lastChild].css("marginRight", freeSpaceLeft + this[childMarginRight]);
+          
+          console.log(`FlexSpaceAroundHelper(run):\n` +
+                      `divWidth = ${divWidth};\n` +
+                      `childrenInOneRow = ${childrenInOneRow};\n` +
+                      `gapsExtraSpacePerChildren = ${gapsExtraSpacePerChildren};\n` +
+                      `missingChildrenInLastRow = ${missingChildrenInLastRow}\n` +
+                      `freeSpaceLeft = ${freeSpaceLeft}`);
+        } else if ( !marginRestored ) {
+          restoreMargin(this);
+          marginRestored = true;
+        }
       }
     }
-  }
-  
+    
+    return FlexSpaceAroundHelper;
+  }());
+      
 }( jQuery ));
